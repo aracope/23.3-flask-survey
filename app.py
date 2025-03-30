b7527d3377fdd293c1bc82f881337d02e8e7737d
aracope <156398622+aracope@users.noreply.github.com>
2025-03-30 16:51:37 -0700

    Step 4 and Step 5 complete, answers handled and thank you page made.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
@@ -25,6 +25,21 @@ def show_question(question_id):
     question = satisfaction_survey.questions[question_id]
 
     return render_template("question.html", question=question, question_id=question_id, survey=satisfaction_survey)
+
+@app.route("/answer", methods=["POST"])
+def handle_answer():
+    """ Save response, redirect to next question. """
+    answer = request.form["answer"]
+    responses.append(answer)
+
+    if len(responses) == len(satisfaction_survey.questions):
+        return redirect(url_for("thank_you"))
+    return redirect(url_for("show_question", question_id=len(responses)))
+
+@app.route("/thank-you")
+def thank_you():
+    """Show completion message"""
+    return render_template("thanks.html")
 
 
 if __name__=="__main__":
