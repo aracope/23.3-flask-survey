1b75055de68b17b5e2557247cfba2e28134f4d66
aracope <156398622+aracope@users.noreply.github.com>
2025-03-31 11:13:59 -0700

    Flash messages when user tries to navigate to already answered, unanswered, or invalid question.

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecretkey'  # Required for flash messages
+
 
 # Initialize the list to store user response
 responses = []
 
 @app.route('/')
 def start_page():
+    """Start page of survey"""
+    global responses
+    responses = [] #Reset responses when restarting survey
     #Info about survey
     title= satisfaction_survey.title
     instructions = satisfaction_survey.instructions
@@ -17,10 +22,11 @@ def start_page():
 
 @app.route("/questions/<int:question_id>")
 def show_question(question_id):
-    """ Show the current question. """
+    """ Show the current question, but prevent accessing invalid or out of order questions. """
     # Ensure the question_id is within a valid range
-    if question_id < len(responses):
+    if question_id != len(responses):
         # if the user tries to access a question they've already answered, redirected
+        flash("Invalid access, please answer questions in order.", "error")
         return redirect(url_for("show_question", question_id=len(responses)))
     
     # If user tries to access a question after the last, redirect to thank you page.
@@ -33,19 +39,19 @@ def show_question(question_id):
 @app.route("/answer", methods=["POST"])
 def handle_answer():
     """ Save response, redirect to next question. """
+    global responses
     answer = request.form["answer"]
     responses.append(answer)
 
     if len(responses) == len(satisfaction_survey.questions):
         return redirect(url_for("thank_you"))
+    
     return redirect(url_for("show_question", question_id=len(responses)))
 
 @app.route("/thank-you")
 def thank_you():
     """Show completion message"""
     return render_template("thanks.html")
-
-
 
 if __name__=="__main__":
     app.run(debug=True)
